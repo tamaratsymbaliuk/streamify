2c6ce6568490a5ea9d77761e1f217982a8fdbb37
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-05-30 21:16:55 -0500

    app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './modules/auth/account/account.module';
+import { SessionModule } from './session/session.module';
 
 @Module({
-  imports: [AccountModule],
+  imports: [AccountModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
