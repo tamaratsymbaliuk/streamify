20587fcc566f5145a72c785ffbc002feb9ca2080
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-06-09 12:59:12 -0500

    added me Query on User

--- server/src/modules/auth/account/account.resolver.ts
+++ server/src/modules/auth/account/account.resolver.ts
@@ -2,6 +2,8 @@ import { Query, Mutation, Args, Resolver } from '@nestjs/graphql';
 import { AccountService } from './account.service';
 import { UserModel } from './models/user.model'
 import { CreateUserInput } from './inputs/create-user.input';
+import { Authorization } from '@/shared/decorators/auth.decorator'
+import { Authorized } from '@/shared/decorators/authorized.decorator'
 
 @Resolver('Account')
 export class AccountResolver {
@@ -15,5 +17,11 @@ export class AccountResolver {
   @Mutation(() => Boolean, { name: 'createUser' }) // all others
   public async create(@Args('data') input: CreateUserInput) {
     return this.accountService.create(input);
+  }
+
+  @Authorization() // 🔐 Guard the route
+  @Query(() => UserModel, { name: 'findProfile' })
+  public async me(@Authorized('id') id: string) {
+    return this.accountService.me(id)
   }
 }
