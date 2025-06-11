f31acb1175266a0244519cc082a2fdb951e20401
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-06-10 18:53:41 -0700

    added metadata to sessionData

--- server/src/shared/types/express-session.d.ts
+++ server/src/shared/types/express-session.d.ts
@@ -1,8 +1,10 @@
 import 'express-session'
+import type { SessionMetadata } from './session-metadata.types'
 
 declare module 'express-session' {
 	interface SessionData {
 		userId?: string
 		createdAt?: Date | string
+		metadata: SessionMetadata
 	}
 }
