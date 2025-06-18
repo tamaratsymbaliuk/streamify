2ebb685bb814619ed24d36aa134d3a234f02fefb
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-06-17 20:06:07 -0500

    updated app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AccountModule } from './modules/auth/account/account.module';
 import { SessionModule } from './session/session.module';
+import { RedisModule } from './core/redis/redis.module';
+import { PrismaModule } from './core/prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AccountModule, SessionModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    RedisModule,
+    AccountModule, 
+    SessionModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
