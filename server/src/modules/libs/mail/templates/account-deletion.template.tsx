bfd038f65c9f4c4b2a59280c6415a730cf2899d8
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-07-08 18:46:49 -0500

    updated text translated to English

--- server/src/modules/libs/mail/templates/account-deletion.template.tsx
+++ server/src/modules/libs/mail/templates/account-deletion.template.tsx
@@ -21,36 +21,36 @@ export function AccountDeletionTemplate({ domain }: AccountDeletionTemplateProps
     return (
         <Html>
             <Head />
-            <Preview>Аккаунт удалён</Preview>
+            <Preview>Account deleted</Preview>
             <Tailwind>
                 <Body className='max-w-2xl mx-auto p-6 bg-slate-50'>
                     <Section className="text-center">
                         <Heading className="text-3xl text-black font-bold">
-                            Ваш аккаунт был полностью удалён
+                            Your account has been completely deleted
                         </Heading>
                         <Text className="text-base text-black mt-2">
-                            Ваш аккаунт был полностью стерт из базы данных Steamify. Все ваши данные и информация были удалены безвозвратно.
+                            Your account has been fully erased from the Steamify database. All your data and information have been permanently deleted.
                         </Text>
                     </Section>
 
                     <Section className="bg-white text-black text-center rounded-lg shadow-md p-6 mb-4">
                         <Text>
-                            Вы больше не будете получать уведомления в Telegram и на почту.
+                            You will no longer receive notifications in Telegram or via email.
                         </Text>
                         <Text>
-                            Если вы захотите вернуться на платформу, вы можете зарегистрироваться по следующей ссылке:
+                            If you'd like to return to the platform, you can register using the following link:
                         </Text>
                         <Link
                             href={registerLink}
                             className="inline-flex justify-center items-center mt-2 text-sm font-medium text-white bg-[#18B9AE] px-5 py-2 rounded-full"
                         >
-                            Зарегистрироваться на Streamify
+                            Register on Streamify
                         </Link>
                     </Section>
 
                     <Section className="text-center text-black">
                         <Text>
-                            Спасибо, что были с нами! Мы всегда будем рады видеть вас на платформе.
+                            Thank you for being with us! We will always be happy to see you on the platform. 
                         </Text>
                     </Section>
                 </Body>
