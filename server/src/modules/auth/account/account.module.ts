68569570a786cff41dd137dbee042a7b0c8b9d8e
tamaratsymbaliuk <tsymbalt14@gmail.com>
2025-07-26 09:14:55 -0500

    account module updated

--- server/src/modules/auth/account/account.module.ts
+++ server/src/modules/auth/account/account.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AccountService } from './account.service';
 import { AccountResolver } from './account.resolver';
+import { VerificationService } from '../verification/verification.service'
+import { PrismaModule } from '@/core/prisma/prisma.module';
+import { VerificationModule } from '../verification/verification.module';
 
 @Module({
-  providers: [AccountResolver, AccountService],
+  imports: [PrismaModule, VerificationModule],
+  providers: [AccountResolver, AccountService, VerificationService],
+  exports: [AccountService]
 })
 export class AccountModule {}
 
